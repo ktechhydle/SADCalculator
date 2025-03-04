68d8e01a5f7bfba9f8fc2dbbe6c21c4a5ecc6f56
Keller Hydle <151480646+ktechhydle@users.noreply.github.com>
2025-03-04 16:54:02 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ import streamlit as st
 st.set_page_config(page_title='Stock Average Down Calculator', page_icon='', layout='wide')
 st.markdown('<h2 style="text-align:center">Stock Average Down Calculator for Day Traders</h2>',
             unsafe_allow_html=True)
+
 layout1, layout2 = st.columns(2)
